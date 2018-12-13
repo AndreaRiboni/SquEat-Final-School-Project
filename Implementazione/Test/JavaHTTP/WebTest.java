9c0dd4716a160fdae6950f3a92de31dbb553fc70
Andrea Riboni <39545859+AndreaRiboni@users.noreply.github.com>
2018-12-13 20:35:43 +0100

    Update WebTest.java

--- Implementazione/Test/JavaHTTP/WebTest.java
+++ Implementazione/Test/JavaHTTP/WebTest.java
@@ -12,13 +12,13 @@ public class WebTest {
 
     public static void main(String[] args) throws IOException {
         System.out.println(
-                WebUtility.getHTMLTagValue(
-                        WebUtility.stringToHTML(
-                                WebUtility.getHTMLPage(
+                WebUtility.getHTMLTagValue( //Voglio ottenere il valore del TAG chiamato "nome"
+                        WebUtility.stringToHTML( //Della pagina HTML contenuta nella stringa
+                                WebUtility.getHTMLPage( //che ho scaricato dall'url
                                         WebUtility.getParametricURL(
-                                                "localhost/HTTP4Java/index.php",
-                                                "nome",
-                                                "andrea"
+                                                "localhost/HTTP4Java/index.php", //(questo url)
+                                                "nome", //chiedendo come parametro il nome
+                                                "andrea" //e passando come valore del parametro nome "andrea"
                                         )
                                 )
                         ),
