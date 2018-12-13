70217dcf7146855b4317ad85bb502dda89465abc
Andrea Riboni <39545859+AndreaRiboni@users.noreply.github.com>
2018-12-13 19:48:04 +0100

    aggiornamento javadoc

--- Implementazione/Test/JavaHTTP/WebTest.java
+++ Implementazione/Test/JavaHTTP/WebTest.java
@@ -1,15 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package webtest;
 
 import java.io.IOException;
 
 /**
  *
- * @author Utente
+ * @author Riboni Andrea
  */
 public class WebTest {
 
