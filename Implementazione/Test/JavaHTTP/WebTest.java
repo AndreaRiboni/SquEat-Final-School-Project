b86d6591d0144acb5c7b91023f2b8663e457b706
Andrea Riboni <39545859+AndreaRiboni@users.noreply.github.com>
2018-12-13 19:47:46 +0100

    rimozione librerie importate e non utilizzate

--- Implementazione/Test/JavaHTTP/WebTest.java
+++ Implementazione/Test/JavaHTTP/WebTest.java
@@ -5,26 +5,7 @@
  */
 package webtest;
 
-import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.logging.Level;
-import java.util.logging.Logger;
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.nodes.Node;
 
 /**
  *
