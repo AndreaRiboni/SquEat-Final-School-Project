98addbf76d0059e56b6cd2f61e93b589a106056b
AndreSapo <45871522+AndreSapo@users.noreply.github.com>
2019-01-26 11:28:07 +0100

    Add files via upload

---
+++ Implementazione/Database/tabelle.sql
@@ -0,0 +1,41 @@
+CREATE TABLE Locale (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(32) NOT NULL,
+    Indirizzo VARCHAR(64) NOT NULL,
+    Cellulare VARCHAR(13) NOT NULL,
+    IDAdmin INT NOT NULL UNIQUE
+);
+
+CREATE TABLE Menù (
+    ID INT PRIMARY KEY,
+    IDLocale INT NOT NULL UNIQUE,
+    IDProdotto INT NOT NULL UNIQUE,
+    Costo FLOAT NOT NULL
+);
+
+CREATE TABLE Prodotto (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(64) NOT NULL,
+    Ingredienti VARCHAR(128) NOT NULL
+);
+
+CREATE TABLE Utente (
+    ID INT PRIMARY KEY,
+    Nome VARCHAR(32) NOT NULL,
+    Cognome VARCHAR(32) NOT NULL,
+    Cellulare VARCHAR(13) NOT NULL UNIQUE,
+    Indirizzo VARCHAR(64) NOT NULL,
+    Mail VARCHAR(64) NOT NULL UNIQUE,
+    Psw VARCHAR(64) NOT NULL,
+    Privilegio INT NOT NULL
+);
+
+CREATE TABLE Ordine (
+    IDOrdine INT PRIMARY KEY,
+    IDCliente INT NOT NULL,
+    IDLocale INT NOT NULL,
+    IDProdotto INT NOT NULL,
+    Giorno DATE NOT NULL,
+    Costo FLOAT NOT NULL
+);
+
